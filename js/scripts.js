f68a8690df9ad18e9a56809eafa572422cedb887
Ben Bennett-Cauchon <bbcauchon@gmail.com>
2019-06-07 12:05:34 -0700

    make name input shake and turn red if submit is clicked with empty input

--- js/scripts.js
+++ js/scripts.js
@@ -20,24 +20,37 @@ function randBetween(min, max) {
 // -----------------------------------------------------------------------------
 // User interface
 // -----------------------------------------------------------------------------
+var forgotNameInputClass = "bg-danger"
+var forgotNameOutputClass = "text-danger"
 $(function(){
   $("form#userInfo").submit(function(event) {
     // Prevent page from reloading
     event.preventDefault();
     // Capture values from form
-    var name = $("#name").val();
+    var name = $("#nameInput").val();
+
     if (!name) {
-      name = "new programmer";
+      name = "Too lazy to type in my name"
+      $("#nameInput").addClass(forgotNameInputClass);
+      $("#nameInput").effect("shake");
+      $("#nameOutput").addClass(forgotNameOutputClass);
+    } else {
+      $("#nameInput").removeClass(forgotNameOutputClass);
+      $("#nameOutput").removeClass(forgotNameOutputClass);
     }
     // Determine recommended language
     var iRandom = randBetween(0, languages.length);
     var recommendedLanguage = languages[iRandom];
     console.log(iRandom, recommendedLanguage);
     // Generate output and show it
-    $("#userName").empty();
-    $("#userName").text(name);
+    $("#nameOutput").empty();
+    $("#nameOutput").text(name);
     $("#suggestedLanguage").empty();
     $("#suggestedLanguage").text(recommendedLanguage);
     $("#output").show();
+  });
+
+  $('#nameInput').on('focus', function(){
+    $(this).removeClass(forgotNameInputClass);
   });
 });
